15f5171b0f63879a9084dd1f826556c122262aff
Dexter Kwok <dkwok.90@gmail.com>
2020-04-26 20:52:05 -0700

    Update comments in Calculator

--- src/functions/Calculator.java
+++ src/functions/Calculator.java
@@ -1,5 +1,15 @@
 package functions;
 
+
+/*
+    Your task:
+    1 - Create a new Calculator<Name> in the package functions
+    2 - Create two functions, a static divide (/) and a non-static multiply (*), and returns the value
+    3 - Create a class called Driver<Name> with a main function in the functions package
+    4 - In the Driver, create your calculator and use the functions which you have created (Use numbers between 1 and 100)
+    6a - If number is greater than 50, print in console -> "Number is greater than 50: <your number>
+    6b - If number is less than 50, print in console -> "Number is less than 50: <your number>"
+ */
 public class Calculator {
 
     public String name;
